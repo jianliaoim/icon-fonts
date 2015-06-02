6b07b132fdeb7d6d6b77fd29b421ed9efbc0bf07
jiyinyiyong <jiyinyiyong@gmail.com>
2015-06-02 16:41:51 +0800

    update layout

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 Standalone icon fonts from TB-UI
 ----
 
+### TODO
+
+Render icons fonts in Canvas and get images back.
+Show images as notification icons.
+
+### Click to Copy
+
+New API available in Chrome 43:
+
 https://developers.google.com/web/updates/2015/04/cut-and-copy-commands?hl=en
+
--- copy.js
+++ copy.js
@@ -1,10 +1,31 @@
 
+var createNotification = function(target) {
+  var content = "Copied: " + target.innerText
+  var notification = new Notification(content)
+  setTimeout(function() {
+    notification.close()
+  }, 1000)
+}
+
 window.addEventListener('click', function(event) {
   if (event.target.className === 'glyphicon-class') {
     var range = document.createRange();
     range.selectNode(event.target);
     window.getSelection().addRange(range);
     var successful = document.execCommand('copy');
+    if (successful) {
+      if (Notification.persmission === 'granted') {
+
+        createNotification(event.target)
+      } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+          if (permission === "granted") {
+            createNotification(event.target)
+          }
+      });
+
+      }
+    }
     window.getSelection().removeAllRanges();
   }
 })
